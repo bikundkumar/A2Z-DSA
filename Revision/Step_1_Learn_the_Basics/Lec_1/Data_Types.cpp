01da6a1a6867a180342def031cd0783874cf5211
“bikundkumar” <kumarbikund2017@gmail.com>
2024-07-26 21:44:12 +0530

    add new file

---
+++ Revision/Step_1_Learn_the_Basics/Lec_1/Data_Types.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+  /*
+  int x = 10;
+  cout <<"int : "<< x << "\n";
+
+  long y = 2017;
+  cout <<"long : "<< y << "\n";
+
+  long long z = 3242;
+  cout <<"long long : "<< z << "\n";
+
+  float a = 23.442;
+  cout <<"float : "<< a << "\n";
+
+  double b = 42.43335;
+  cout <<"double : "<< b << "\n";
+
+  char  ch = 'a';
+  cout <<"char : "<< ch << "\n";
+  
+  string st = "Hello Coders";
+  cout <<"string : "<< st << "\n";
+  */
+
+  // Data Size //
+
+  int a;
+  long b;
+  long long c;
+  float d;
+  double e;
+  char f;
+  string g;
+
+  cout <<"Size of int = " << sizeof(int) <<" Bytes\n";
+  cout <<"Size of long = " << sizeof(long) <<" Bytes\n";
+  cout <<"Size of long long = " << sizeof(long long) <<" Bytes\n";
+  cout <<"Size of float = " << sizeof(float) <<" Bytes\n";
+  cout <<"Size of double = " << sizeof(double) <<" Bytes\n";
+  cout <<"Size of char = " << sizeof(char) <<" Bytes\n";
+  cout <<"Size of string = " << sizeof(string) <<" Bytes\n";
+
+
+  return 0;
+}
