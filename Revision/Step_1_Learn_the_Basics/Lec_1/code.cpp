06c281fe39ffa70b7e6a664d5124dd2586b1fe26
“bikundkumar” <kumarbikund2017@gmail.com>
2024-07-26 20:00:37 +0530

    code.cpp

---
+++ Revision/Step_1_Learn_the_Basics/Lec_1/code.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+  /* cout <<"Hello, Coders!\n";
+  cout <<"Hello, Coders!\n"; */
+
+  /*cout <<"Hello, Coders!"<<endl;
+  cout <<"Hello, Coders!"<<endl; */ 
+  
+  int x;
+  int y;
+  cin >>x;
+  cin >>y;
+  cout <<"Value of x : " << x <<" and y : " << y <<"\n";
+
+    return 0;
+} 
