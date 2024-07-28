4cec9613a4812ff700e77d4364a28c340bd9acba
“bikundkumar” <kumarbikund2017@gmail.com>
2024-07-28 13:05:40 +0530

    ignore a.out

---
+++ Revision/Step-1-Learn-the-Basics/Lec-1/for-Loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+  for(int i = 0; i < 10; i++) {
+    cout <<"Hello Coders, this is the " << i <<"th iteration\n";
+  }
+
+
+
+
+
+
+  return 0;
+}
---
+++ Revision/Step-1-Learn-the-Basics/Lec-1/strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+  string s = "Bikund Kumar";
+
+  cout << s[0] <<s[1] <<s[2] <<s[3] <<s[4] <<s[5] <<s[6] <<s[7] <<s[8] <<s[9] <<s[10] <<s[11] << endl;
+
+
+  return 0;
+}
