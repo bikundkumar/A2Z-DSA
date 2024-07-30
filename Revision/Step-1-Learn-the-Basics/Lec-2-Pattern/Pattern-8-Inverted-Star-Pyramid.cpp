11a79aa27fffd99105f80e51af5f58536144e732
“bikundkumar” <kumarbikund2017@gmail.com>
2024-07-31 05:04:14 +0530

    add new file

---
+++ Revision/Step-1-Learn-the-Basics/Lec-2-Pattern/Pattern-8-Inverted-Star-Pyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void Pattern8(int n) {
+  for (int i = 0; i < n; i++) {
+
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    for (int j = 0; j < 2*n - (2*i + 1); j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+
+  int n = 3;
+  Pattern8(n);
+
+
+  return 0;
+}
