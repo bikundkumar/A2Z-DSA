741e29d7365a7318392bb211c233dfb71c933e59
“bikundkumar” <kumarbikund2017@gmail.com>
2024-08-09 07:31:26 +0530

    add new files

---
+++ Step-1-Learn-the-Basics/Lec2-Pattern/Pattern-13-Increasing-Number-Triangle-Pattern.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void Pattern13(int n) {
+  int num = 1;
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << num << " ";
+      num  = num + 1;
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  int t;
+  cout <<"enter t : ";
+  cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    int n;
+    cout <<"enter n : ";
+    cin >> n;
+    Pattern13(n);
+  }
+
+  return 0;
+}
